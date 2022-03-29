fcf7dd7967d76d8fcebeaa92b144ca9e5ca31f05
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-03-29 12:31:27 -0400

    Added JEST testing functions.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,10 +4,9 @@ import React, { useState } from 'react';
 
 export default function Form(props) {
 
-  console.log(props.interviewer);
-
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setStudent("");
@@ -19,19 +18,30 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name="student"
             type="text"
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers}
           value={interviewer}
@@ -41,7 +51,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => {props.onSave(student, interviewer)}}>Save</Button>
+          <Button confirm onClick={() => {validate()/* props.onSave(student, interviewer) */}}>Save</Button>
         </section>
       </section>
     </main>
